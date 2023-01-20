8f0f56f1d8b0b5f7cf804425d163d1a08e4dbeca
onomeh <onwuanibeonome@gmail.com>
2023-01-20 23:49:59 +0000

    Create method for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  def recent_comments
+    comments.order(created_at: :DESC).limit(5)
+  end
+
+  def update_post_counter
+    user.update(posts_counter: user.posts.count)
+  end
 end
