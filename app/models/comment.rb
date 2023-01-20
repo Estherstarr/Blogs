189002edb0222c2eb4d714b7f0277b339c0f1617
onomeh <onwuanibeonome@gmail.com>
2023-01-20 23:49:34 +0000

    Create method for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  def update_comment_counter
+    Post.find(post_id).update(comments_counter: Post.find(post_id).comments.count)
+  end
 end
