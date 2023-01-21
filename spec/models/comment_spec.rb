ee54415a2dc8d65ce573c2197520252f493879ba
onomeh <onwuanibeonome@gmail.com>
2023-01-21 20:45:54 +0000

    Fix test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:author) { User.create(name: 'Onome') }
-  let(:post) { Post.create(author: author, title: 'This is my first blog', text: 'Welcome to home') }
+  let!(:author) { User.create(name: 'Onome') }
+  let!(:post) { Post.create(author: author, title: 'This is my first blog', text: 'Welcome to home') }
 
   describe 'update comment counter' do
     before { Comment.create(post: post, author: author, text: 'Nice blog') }
