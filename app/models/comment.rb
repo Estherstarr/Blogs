3ac0014606340e089de3fa60546610f9b56d59cc
onomeh <onwuanibeonome@gmail.com>
2023-01-21 18:44:21 +0000

    Update methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :update_comment_counter
+
+  private
+
   def update_comment_counter
-    Post.find(post_id).update(comments_counter: Post.find(post_id).comments.count)
+    post.increment!(:comments_counter)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,11 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :update_likes_counter
+
+  private
+
   def update_likes_counter
-    Post.find(post_id).update(likes_counter: Post.find(post_id).likes.count)
+    post.increment!(:likes_counter)
   end
 end
