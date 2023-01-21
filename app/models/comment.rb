b2324c946f9fe950a67c623b371a1368bc15eb34
onomeh <onwuanibeonome@gmail.com>
2023-01-21 18:53:36 +0000

    Fix linters error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
-  
+
   validates :text, presence: true
-  
+
   after_save :update_comment_counter
 
   private
