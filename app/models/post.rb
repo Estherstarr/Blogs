220f08664d3f015f6dcbc9b1e24eed02fec58133
onomeh <onwuanibeonome@gmail.com>
2023-01-21 11:38:46 +0000

    Use after save

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,15 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  after_save :update_post_counter
+
   def recent_comments
     comments.order(created_at: :DESC).limit(5)
   end
 
+  private
+
   def update_post_counter
-    user.update(posts_counter: user.posts.count)
+    author.increment!(:posts_counter)
   end
 end
