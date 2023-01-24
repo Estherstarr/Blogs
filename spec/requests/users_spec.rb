3a10cc2e8a943416d9a9330c50fdc9ee6d710ef3
onomeh <onwuanibeonome@gmail.com>
2023-01-24 02:37:25 +0000

    Create request spec for users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
+  describe 'GET #index' do
+    let(:request) { get users_path }
+
     it 'returns http success' do
-      get '/users/index'
+      request
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      request
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes correct placeholder text' do
+      request
+      expect(response.body).to include("Here's a list of Users")
     end
   end
 
-  describe 'GET /show' do
+  describe 'GET #show' do
     it 'returns http success' do
-      get '/users/show'
+      get user_path(id: 1)
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      get user_path(id: 1)
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text' do
+      get user_path(id: 1)
+      expect(response.body).to include('Post 3')
     end
   end
 end
