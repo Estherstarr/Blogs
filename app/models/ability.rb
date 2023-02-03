3156daf8e93d3da874f35f2c26afa000937dc0bd
onomeh <onwuanibeonome@gmail.com>
2023-02-03 23:16:41 +0000

    Implement authorization

---
+++ app/models/ability.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, Post, public: true
+
+    can :read, Comment, public: true
+
+    return unless user.present?
+
+    can [:read, :destroy], Post, user: user
+
+    can [:read, :destroy], Comment, user: user
+
+    return unless user.admin?
+
+    can [:read, :destroy], Post
+
+    can [:read, :destroy], Comment
+
+    # Define abilities for the user here. For example:
+    #
+    #   return unless user.present?
+    #   can :read, :all
+    #   return unless user.admin?
+    #   can :manage, :all
+    #
+    # The first argument to `can` is the action you are giving the user
+    # permission to do.
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on.
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, published: true
+    #
+    # See the wiki for details:
+    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+  end
+end
